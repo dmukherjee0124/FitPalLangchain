258a46f7404e3920cc812719d15b7e5edc9b74db
dim16 <dim16@aber.ac.uk>
2024-02-25 22:51:36 +0000

    Initial commit

---
+++ app/conversation.py
@@ -0,0 +1,95 @@
+
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+import os
+load_dotenv()
+
+api_key=os.getenv("api_key")
+
+
+def conversation(age, goal, level, days, weeks, time, style, gym, health, current_week):
+    llm = ChatOpenAI(openai_api_key=api_key)
+    output_concat = ""
+    template = """
+                        Can you create a fitness routine from the below criterias
+
+                        Age: {age}
+
+                        Primary Fitness Goals: {goal}
+
+                        Current Fitness Level: {level}
+
+                        Number of sessions each week:{days}
+                        
+                        Till how many number of weeks: {weeks} 
+
+                        Time Commitment: {time}
+
+                        Preferred Workout Style(e.g., strength training, cardio, flexibility, HIIT): {style}
+
+                        Equipment Availability (Access to gym and equipments or at home with minimal equipment):{gym}
+
+                        Any Health Considerations:{health}
+
+                        Create the first week in the form of - Week 1: Day 1: workout ...etc 
+                        (Dont give any extra information in the end)
+
+                        """
+    promp = PromptTemplate(
+        input_variables=['age', 'goal', 'level', 'days', 'weeks','time', 'style', 'gym', 'health'],
+        template=template
+    )
+
+    chain = LLMChain(llm=llm, prompt=promp,verbose=True)
+    output = chain.invoke(
+        {'age': age, 'goal': goal, 'level': level, 'days': days,'weeks':weeks, 'time': time, 'style': style, 'gym': gym,
+         'health': health})
+    output_concat = output_concat + output.get("text")
+
+    week_total = int(weeks)
+    current_week=int(current_week)
+    if week_total > 1:
+        while current_week<= week_total:
+            llm = ChatOpenAI(openai_api_key=api_key)
+            template = """
+                                    Create training program for week number {current_week} given information about last weeks training:
+                                    {program}
+
+                                    Age: {age}
+
+                                    Primary Fitness Goals: {goal}
+
+                                    Current Fitness Level: {level}
+
+                                    Number of sessions each week:{days}
+
+                                    Time Commitment: {time}
+
+                                    Preferred Workout Style(e.g., strength training, cardio, flexibility, HIIT): {style}
+
+                                    Equipment Availability (Access to gym and equipments or at home with minimal equipment):{gym}
+
+                                    Any Health Considerations:{health}
+
+                                    Create the week in the form of - Week {current_week}: Day 1: workout ...etc 
+                                    Make the program more challenging than the previous week.
+                                    (Dont give any extra information in the end)
+
+                                    """
+            promp = PromptTemplate(
+                input_variables=['age', 'goal', 'level', 'days', 'weeks', 'current_week', 'time', 'style', 'gym', 'health'],
+                template=template
+            )
+            chain = LLMChain(llm=llm, prompt=promp, verbose=True)
+            output = chain.invoke(
+                {'program':output,'age': age, 'goal': goal, 'level': level, 'days': days, 'weeks': weeks, 'current_week': current_week, 'time': time, 'style': style,
+                 'gym': gym, 'health': health})
+            current_week = current_week + 1
+            output_concat = output_concat + "\n" + output.get("text")
+
+    return output_concat
+
+
+
---
+++ app/main.py
@@ -0,0 +1,48 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from fastapi import FastAPI
+from langcorn import create_service
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, errors
+from app.conversation import conversation
+
+class Input(BaseModel):
+    # human_input: str
+    age: str
+    goal: str
+    level: str
+    days: str
+    weeks:str
+    time: str
+    style: str
+    gym: str
+    health: str
+    current_week:str
+
+class Output(BaseModel):
+    output: str
+
+app=FastAPI()
+
+@app.post("/conversation")
+async def input(input: Input):
+    output = Output(output=conversation(input.age, input.goal, input.level, input.days, input.weeks, input.time, input.style, input.gym, input.health,input.current_week))
+    return output
+
+origins = [
+    "<http://localhost>",
+    "<http://localhost:5173>",
+        "...Your Domains..."
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
